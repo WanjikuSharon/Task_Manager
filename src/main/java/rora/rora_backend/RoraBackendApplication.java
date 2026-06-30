92e8492c8ddbea944969d9e1b7c186a9a2b79d9e
SharonWanjiku <sharonwanjiku292@gmail.com>
2026-06-30 15:59:12 +0300

    Task Manager in Java21

---
+++ src/main/java/rora/rora_backend/RoraBackendApplication.java
@@ -0,0 +1,13 @@
+package rora.rora_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoraBackendApplication.class, args);
+	}
+
+}
